483bd4ab011e8366c4ad2c9fb9c2f3e3a0520866
clank201 <clank201@gmail.com>
2015-05-31 04:47:14 +0200

    Ported to sf2dlib Also added random "rocks"

--- source/main.cpp
+++ source/main.cpp
@@ -4,182 +4,238 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
-#include "functions.c"
-#include "dwarf_bin.h"
-#include "field_bin.h"
-#include "wall_bin.h"
+#include <math.h>
+#include <sf2d.h>
+#include <time.h>
 
+extern "C" {
+	extern const struct {
+		unsigned int 	 width;
+		unsigned int 	 height;
+		unsigned int 	 bytes_per_pixel;
+		unsigned char	 pixel_data[];
+	} dwarf_img;
+
+	extern const struct {
+		unsigned int 	 width;
+		unsigned int 	 height;
+		unsigned int 	 bytes_per_pixel;
+		unsigned char	 pixel_data[];
+	} wall_img;
+
+	extern const struct {
+		unsigned int 	 width;
+		unsigned int 	 height;
+		unsigned int 	 bytes_per_pixel;
+		unsigned char	 pixel_data[];
+	} field_img;
+}
 struct entity{
-	u8* spriteData;
+	sf2d_texture *spriteData;
 	int spriteHeight;
 	int spriteWidth;
 	int posX;
 	int posY;
 	bool tangible;
+	bool divertit;
 };
 
-void cameraOperation(entity player, int &cameraX, int &cameraY,const int mapHeight,const int mapWidth){
-	if (player.posX < mapWidth-10){ if (cameraX + 14 < player.posX){ cameraX++; } }
+void cameraOperation(entity player, int &cameraX, int &cameraY, const int mapHeight, const int mapWidth){
+	if (player.posX < mapWidth - 10){ if (cameraX + 14 < player.posX){ cameraX++; } }
 	if (player.posX > 9){ if (cameraX + 10>player.posX){ cameraX--; } }
-	if (player.posY < mapHeight-6){ if (cameraY + 8 < player.posY){ cameraY++; } }
+	if (player.posY < mapHeight - 6){ if (cameraY + 8 < player.posY){ cameraY++; } }
 	if (player.posY > 5){ if (cameraY + 6>player.posY){ cameraY--; } }
 }
 
 int main()
 {
-	// Initializations
-	srvInit();        // services
-	aptInit();        // applets
-	hidInit(NULL);    // input
-	gfxInitDefault();//initialize GPU
+	// Inits
+	srvInit();
+	aptInit();
+	hidInit(NULL);
+
+	srand(time(NULL));
+
+	sf2d_init();
+	//gfxInitDefault();
 	consoleInit(GFX_BOTTOM, NULL);
-	u32 kDown;        // keys down
-	u32 kHeld;        // keys pressed
-	u32 kUp;          // keys up
+	sf2d_set_clear_color(RGBA8(0x40, 0x40, 0x40, 0xFF));
+
+	sf2d_texture *tex1 = sf2d_create_texture(dwarf_img.width, dwarf_img.height, GPU_RGBA8, SF2D_PLACE_RAM);
+	sf2d_fill_texture_from_RGBA8(tex1, dwarf_img.pixel_data, dwarf_img.width, dwarf_img.height);
+	sf2d_texture_tile32(tex1);
+
+	sf2d_texture *tex2 = sf2d_create_texture(wall_img.width, wall_img.height, GPU_RGBA8, SF2D_PLACE_RAM);
+	sf2d_fill_texture_from_RGBA8(tex2, wall_img.pixel_data, wall_img.width, wall_img.height);
+	sf2d_texture_tile32(tex2);
+
+	sf2d_texture *tex3 = sf2d_create_texture(field_img.width, field_img.height, GPU_RGBA8, SF2D_PLACE_RAM);
+	sf2d_fill_texture_from_RGBA8(tex3, field_img.pixel_data, field_img.width, field_img.height);
+	sf2d_texture_tile32(tex3);
+
+	u32 kDown;
+	u32 kHeld;
+	u32 kUp;
 	int cameraY = 0;
 	int cameraX = 0;
 	int temp = 0;
 	int npcN = 0;
-	const int mapHeight=1000;
-	const int mapWidth=1000;
+	const int mapHeight = 1000;
+	const int mapWidth = 1000;
 	static entity* map[mapWidth][mapHeight];
 
 	entity player;
-	player.spriteData = (u8*)dwarf_bin;
+	player.spriteData = tex1;
 	player.spriteHeight = 16;
 	player.spriteWidth = 16;
 	player.tangible = 1;
+	player.divertit = 0;
 
 	entity NPC1;
-	NPC1.spriteData = (u8*)dwarf_bin;
+	NPC1.spriteData = tex1;
 	NPC1.spriteHeight = 16;
 	NPC1.spriteWidth = 16;
 	NPC1.tangible = 1;
 	NPC1.posX = 3;
-	NPC1.posY= 3;
-
+	NPC1.posY = 3;
 	entity field;
-	field.spriteData = (u8*)field_bin;
+	field.spriteData = tex3;
 	field.spriteHeight = 16;
 	field.spriteWidth = 16;
 	field.tangible = 0;
+	field.divertit = 1;
 
 	entity wall;
-	wall.spriteData = (u8*)wall_bin;
+	wall.spriteData = tex2;
 	wall.spriteHeight = 16;
 	wall.spriteWidth = 16;
 	wall.tangible = 1;
+	wall.divertit = 0;
 
 	for (int i = 0; i != mapHeight; i++){
 		for (int j = 0; j != mapWidth; j++){
 			map[j][i] = &wall;
 		}
 	}
-	for (int i = 1; i != mapHeight-1; i++){
-		for (int j = 1; j != mapWidth-1; j++){
+	for (int i = 1; i != mapHeight - 1; i++){
+		for (int j = 1; j != mapWidth - 1; j++){
 			map[j][i] = &field;
+		}
+	}
+	for (int i = 1; i != mapHeight - 1; i++){
+		for (int j = 1; j != mapWidth - 1; j++){
+			if (rand() % 20 == 0){ map[j][i] = &wall; }
 		}
 	}
 
 	player.posX = 5;
 	player.posY = 5;
-	// Main loop
+	map[5][5] = &player;
+
 	while (aptMainLoop()){
 		//PREPARATIUS
-		// Wait for next frame
-		gspWaitForVBlank();
 
+		sf2d_swapbuffers();
 		//INPUTS
-		// Read which buttons are currently pressed or not
-		hidScanInput();
+
+		if (temp == 0){ hidScanInput(); }
 		kDown = hidKeysDown();
 		kHeld = hidKeysHeld();
 		kUp = hidKeysUp();
 
-		// If START button is pressed, break loop and quit
-		if (kHeld & KEY_START){
+		if (kDown & KEY_START){
 			break;
 		}
 		if (temp == 0){
-			if (kHeld & KEY_DOWN){
+			if (kHeld & KEY_UP){
 				if (map[player.posX][player.posY - 1]->tangible == 0){
 					map[player.posX][player.posY] = &field;
 					player.posY--;
-					temp = 2;
+					map[player.posX][player.posY] = &player;
 				}
 			}
 			if (kHeld & KEY_LEFT){
 				if (map[player.posX - 1][player.posY]->tangible == 0){
 					map[player.posX][player.posY] = &field;
 					player.posX--;
-					temp = 2;
+					map[player.posX][player.posY] = &player;
 				}
 			}
 			if (kHeld & KEY_RIGHT){
 				if (map[player.posX + 1][player.posY]->tangible == 0){
 					map[player.posX][player.posY] = &field;
 					player.posX++;
-					temp = 2;
+					map[player.posX][player.posY] = &player;
 				}
 			}
-			if (kHeld & KEY_UP){
+			if (kHeld & KEY_DOWN){
 				if (map[player.posX][player.posY + 1]->tangible == 0){
 					map[player.posX][player.posY] = &field;
 					player.posY++;
-					temp = 2;
+					map[player.posX][player.posY] = &player;
+
 				}
 			}
 		}
-		
-		//CODI
-		if (npcN == 2){
-			if (map[NPC1.posX][NPC1.posY - 1]->tangible == 0){
-				map[NPC1.posX][NPC1.posY] = &field;
-				NPC1.posY--;			
-			}
-		}
-		else if (npcN == 1){
-			if (map[NPC1.posX - 1][NPC1.posY]->tangible == 0){
-				map[NPC1.posX][NPC1.posY] = &field;
-				NPC1.posX--;				
-			}
-		}
-		else if (npcN == 3){
-			if (map[NPC1.posX + 1][NPC1.posY]->tangible == 0){
-				map[NPC1.posX][NPC1.posY] = &field;
-				NPC1.posX++;
-				
-			}
-		}
-		else if (npcN==0){
-			if (map[NPC1.posX][NPC1.posY + 1]->tangible == 0){
-				map[NPC1.posX][NPC1.posY] = &field;
-				NPC1.posY++;
-			}
-		}
-		
-		if (npcN ==3){
-			npcN = 0;
-		}
-		else { npcN++; }
-		std::cout << "NPC1.posX==" << NPC1.posX << std::endl << "NPC1.posY==" << NPC1.posY << std::endl;
-		std::cout << "player.posX==" << player.posX << std::endl << "player.posY==" << player.posY << std::endl;
-		if (temp > 0){ temp--; }
-		//OUTPUT
-		map[player.posX][player.posY] = &player;
-		map[NPC1.posX][NPC1.posY] = &NPC1;
-		cameraOperation(player, cameraX, cameraY,mapHeight,mapWidth);
 
+		int rng1 = rand() % 4;
+		if (temp == 0 & rand() % 3 == 0){ //si el tick es 0, una de cada 3 vegades(probabilisticament)
+			switch (rng1){
+			case 2:
+				if (map[NPC1.posX][NPC1.posY - 1]->tangible == 0){
+					map[NPC1.posX][NPC1.posY] = &field;
+					NPC1.posY--;
+					map[NPC1.posX][NPC1.posY] = &NPC1;
+				}
+				break;
+
+			case 1:
+				if (map[NPC1.posX - 1][NPC1.posY]->tangible == 0){
+					map[NPC1.posX][NPC1.posY] = &field;
+					NPC1.posX--;
+					map[NPC1.posX][NPC1.posY] = &NPC1;
+				}
+				break;
+
+			case 3:
+				if (map[NPC1.posX + 1][NPC1.posY]->tangible == 0){
+					map[NPC1.posX][NPC1.posY] = &field;
+					NPC1.posX++;
+					map[NPC1.posX][NPC1.posY] = &NPC1;
+				}
+				break;
+
+			case 0:
+				if (map[NPC1.posX][NPC1.posY + 1]->tangible == 0){
+					map[NPC1.posX][NPC1.posY] = &field;
+					NPC1.posY++;
+					map[NPC1.posX][NPC1.posY] = &NPC1;
+				}
+				break;
+			}
+		}
+
+		if (temp > 0){ temp--; }
+		else { temp = 4; }
+		//OUTPUT
+		cameraOperation(player, cameraX, cameraY, mapHeight, mapWidth); //Moure la camera
+
+		sf2d_start_frame(GFX_TOP, GFX_LEFT);
 		for (int i = 0; i != 15; i++){
 			for (int j = 0; j != 25; j++){
-				gfxDrawSprite(GFX_TOP, GFX_LEFT, map[j + cameraX][i + cameraY]->spriteData, 16, 16, i * 16, j * 16);
+				sf2d_draw_texture(map[j + cameraX][i + cameraY]->spriteData, j * 16, i * 16);
 			}
 		}
-		gfxFlushBuffers();
-		gfxSwapBuffers();
+		sf2d_end_frame();
+
 	}
 
 	// Exit
+	sf2d_free_texture(tex1);
+	sf2d_free_texture(tex2);
+	sf2d_free_texture(tex3);
+
+	sf2d_fini();
 	gfxExit();
 	hidExit();
 	aptExit();
